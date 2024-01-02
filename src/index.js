77ffb6510728618cc1931f8c4232ee09eb4a8598
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2024-01-02 21:15:35 +0200

    feat: add optimizations

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 const connectDb = require('./utils/db');
-const Chat = require('./models/Chat');
 const chatRoutes = require('./routes/chat');
 const userRoutes = require('./routes/user');
 const roomRoutes = require('./routes/room');
 const errorHandler = require('./middlewares/error');
+const handleSocketConnection = require('./utils/socket');
 
 connectDb();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/chats', chatRoutes);
 app.use('/api/users', userRoutes);
@@ -22,33 +24,7 @@ app.use('/api/rooms', roomRoutes);
 const server = http.createServer(app);
 const io = socketIo(server);
 
-io.on('connection', (socket) => {
-  socket.on('message', async({ sender, message, room }) => {
-    try {
-      const chat = new Chat({
-        sender,
-        message,
-        room,
-      });
-
-      await chat.save();
-
-      if (room) {
-        socket.to(room).emit('message', chat);
-      } else {
-        // send the message to all connected clients
-        socket.broadcast.emit('message', chat);
-      }
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error(err);
-    }
-  });
-
-  socket.on('join room', (room) => {
-    socket.join(room);
-  });
-});
+io.on('connection', handleSocketConnection);
 
 // Error handling middleware
 app.use(errorHandler);
---
+++ src/utils/socket.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Chat = require('../models/Chat');
+
+module.exports = (socket) => {
+  socket.on('message', async({ sender, message, room }) => {
+    try {
+      const chat = new Chat({
+        sender,
+        message,
+        room,
+      });
+
+      await chat.save();
+
+      if (room) {
+        socket.to(room).emit('message', chat);
+      } else {
+        // send the message to all connected clients
+        socket.broadcast.emit('message', chat);
+      }
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+    }
+  });
+
+  socket.on('join room', (room) => {
+    socket.join(room);
+  });
+};
