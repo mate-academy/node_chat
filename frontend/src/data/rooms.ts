22462e40731e687cc8e07817f915cff7e3434400
Volodymyr Voloshyn <riokin2011@gmail.com>
2024-01-30 12:39:03 +0100

    Delete fake

--- frontend/src/data/rooms.ts
+++
@@ -1,7 +0,0 @@
-import { Room } from "../types/room";
-
-export const rooms: Room[] = [
-  { id: "1", name: "Cars", color: "red" },
-  { id: "2", name: "Pets", color: "green" },
-  { id: "3", name: "Dogs", color: "blue" },
-];
