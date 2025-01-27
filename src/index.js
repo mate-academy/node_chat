8494ae0a624a2e0e38d52b2a2f6898601ac9349a
luter <newgglutt@gmail.com>
2025-01-27 23:38:44 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Порт
+const port = process.env.PORT || 3005;
+
+// Роут
+app.get('/', (req, res) => {
+  res.send('Hello bro, server is listening!');
+});
+
+// Запуск сервера
+app.listen(port, () => {
+  // console.log(`Server started on http://localhost:${port}`);
+});
