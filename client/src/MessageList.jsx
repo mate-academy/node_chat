7b0cff4de62870a6ab6691da04281674b04f7a3f
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-08-09 14:53:52 +0200

    Delete client directory

--- client/src/MessageList.jsx
+++
@@ -1,9 +0,0 @@
-export const MessageList = ({ messages }) => (
-  <ul>
-    {messages.map(message => (
-      <li key={message.time}>
-        <b>{message.author}</b> [{new Date(message.time).toLocaleTimeString()}]: {message.text}
-      </li>
-    ))}
-  </ul>
-);
