485e5e5a60bf56cd7254707fddd7915033a7756c
Alyona Shunevych <olena17ju06@gmail.com>
2026-03-31 13:12:56 +0300

    Solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,26 +6,5 @@ module.exports = {
   rules: {
     'no-proto': 0
   },
-  plugins: ['jest'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      extends: [
-        '@mate-academy/eslint-config',
-        'plugin:@typescript-eslint/recommended'
-      ],
-      rules: {
-        'no-proto': 0,
-        '@typescript-eslint/no-unused-vars': 'error'
-      }
-    }
-  ]
+  plugins: ['jest']
 };
