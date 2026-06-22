d6bb92876847ce859f95c355cf03e1e643dde9e8
Yaroslav <yaroslavhohulia@gmail.com>
2026-06-22 15:05:39 +0300

    fix3

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
   extends: '@mate-academy/eslint-config',
   env: {
-    jest: true
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
   },
   rules: {
-    'no-proto': 0
+    'no-proto': 0,
   },
-  plugins: ['jest']
+  plugins: ['jest'],
 };
