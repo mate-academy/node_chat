474726e9e0ce2198b1d66998aace9d72711df709
Yana <utrsach@gmail.com>
2026-08-22 15:29:25 +0300

    Fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,4 @@
 import ReactDOM from 'react-dom/client';
 import 'bulma/css/bulma.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
